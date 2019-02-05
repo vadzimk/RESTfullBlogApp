f2d0f921d1da86507ee7147d610171f48b1db31a
Vadzim <vadzimk@hotmail.com>
2019-02-04 23:22:19 -0800

    initial commit

--- app.js
+++ app.js
@@ -5,12 +5,12 @@
 
 // app config
 mongoose.connect("mongodb://localhost/restful_blog_app");
-app.set("view engine", "ejs");
+app.set("view engine", "ejs"); //so that we don't need to type .ejs when render
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
 //Mongoose/model config
-var blobSchema = new mongoose.Schema({
+var blogSchema = new mongoose.Schema({
 	title: String,
 	image: {type: String, default: "placeholderimage.jpeg"},
 	body: String,
@@ -19,7 +19,28 @@ var blobSchema = new mongoose.Schema({
 
 var Blog = mongoose.model("Blog", blogSchema);
 
+Blog.create({
+	title: "Test Blog",
+	image: "https://unsplash.com/photos/K4mSJ7kc0As",
+	body: "hello this is a blog-post"
+});
+
 // Routs (Restful routs)
+
+app.get("/", function(req, res){
+	res.redirect("/blogs");
+});
+app.get("/blogs", function(req, res){
+	Blog.find({}, function(err, blogs){
+		if(err){
+			console.log(err);
+		}else{
+			res.render("index.ejs", {blogs: blogs});
+		}
+	})
+	
+});
+// we need a template called index.ejs in the views directory
 
 
 
